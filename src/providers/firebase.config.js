73be70c35a35c3bfb72dc87a2ce8d567733eaa34
Samir Sadek <samirsadek2009@gmail.com>
2023-11-08 20:08:24 +0600

    firebase config env file

--- src/providers/firebase.config.js
+++ src/providers/firebase.config.js
@@ -5,12 +5,12 @@ import { initializeApp } from "firebase/app";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyBJNU6xOth3SYa17Cwt5Rub1-89tkZP2tE",
-  authDomain: "online-marketplace-481d3.firebaseapp.com",
-  projectId: "online-marketplace-481d3",
-  storageBucket: "online-marketplace-481d3.appspot.com",
-  messagingSenderId: "488087144461",
-  appId: "1:488087144461:web:8e1aedc4e6cb7e4edaed44"
+  apiKey:import.meta.env.VITE_APIKEY,
+  authDomain:import.meta.env.VITE_AUTHDOMAIN,
+  projectId:import.meta.env.VITE_PROJECTID,
+  storageBucket:import.meta.env.VITE_STORAGEBUCKET,
+  messagingSenderId:import.meta.env.VITE_MESSAGINGSENDERID,
+  appId:import.meta.env.VITE_APPID,
 };
 
 // Initialize Firebase
